e894dc4b85a21f658ca75f56590dd31d90a2066e
matthewfreire <matt@justdjango.com>
2021-09-29 00:02:27 +0100

    Making requests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import axios from 'axios'
 
 function App() {
+
+  useEffect(() => {
+
+    async function pingAPI() {
+      return await axios.get("https://api.coingecko.com/api/v3/ping")
+        .then(res => {
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+
+    pingAPI()
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
     </div>
   );
 }
