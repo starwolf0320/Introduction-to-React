e24c37946554b86fc16d7b08d176e447e3cb89b5
matthewfreire <matt@justdjango.com>
2021-09-29 00:25:08 +0100

    calling api using coinbase package

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import { useEffect } from "react";
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import CoinGecko from 'coingecko-api'
 
 function App() {
+  const [coins, setCoins] = useState(null)
 
   useEffect(() => {
+    const CoinGeckoClient = new CoinGecko();
 
     async function pingAPI() {
-      return await axios.get("https://api.coingecko.com/api/v3/ping")
-        .then(res => {
-          console.log(res.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+      return await CoinGeckoClient.ping();
+    }
+
+    async function fetchCoinList() {
+      const response = await CoinGeckoClient.coins.all()
+      setCoins(response.data)
+      return response
     }
 
     pingAPI()
+    fetchCoinList()
 
   }, [])
 
   return (
     <div>
-      Hello World
+      {coins && (
+        <ul>
+          {coins.map(coin => {
+            return (
+              <li key={coin.id}>{coin.id}</li>
+            )
+          })}
+        </ul>
+      )}
     </div>
   );
 }
